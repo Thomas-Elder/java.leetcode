b6ca53849d1114fc8830f68dcab57211970e6470
Thomas <thombsaway@gmail.com>
2023-08-13 10:51:07 +1000

    Added Utility package and ListNode class.

---
+++ LeetCode/src/main/java/Utility/ListNode.java
@@ -0,0 +1,12 @@
+package Utility;
+
+import java.util.List;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int value){
+        val = value;
+    }
+}
